22704c16fc9aa7baf43ad44cb9b4ecab93a9fa89
Eliezer Marques <neto15.cg@gmail.com>
2021-03-06 14:00:10 -0300

    Bug cors netlify 4

--- webpack.config.js
+++ webpack.config.js
@@ -74,6 +74,11 @@ module.exports = {
     contentBase: path.join(__dirname, 'public'),
     port: 9999,
     disableHostCheck: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
   },
   plugins: [
     new Dotenv(),
