75ddc7a5deceefcab80d73c47dc52e084337a76c
Eliezer Marques <neto15.cg@gmail.com>
2021-03-07 18:06:06 -0300

    Cria teste 2:2 para pagina de produtos

--- cypress/integration/Products.spec.js
+++ cypress/integration/Products.spec.js
@@ -1,6 +1,7 @@
 context('Products', () => {
   beforeEach(() => {
-    cy.visit('https://delivery-challenge.netlify.app/products?lat=-23.6317641&lng=-46.7014641');
+    // cy.visit('https://delivery-challenge.netlify.app/products?lat=-23.6317641&lng=-46.7014641');
+    cy.visit('http://localhost:9999/products?lat=-23.6317641&lng=-46.7014641');
   });
 
   it('https://delivery-challenge.netlify.app/products?lat=-23.6317641&lng=-46.7014641', () => {
@@ -15,6 +16,13 @@ context('Products', () => {
     cy.contains('Skol 269ml - Unidade');
     cy.get('[data-testid=increment-button]').first().click();
     cy.get('[data-testid=button-card-0]').click();
-    // cy.get('[data-testid=increment-button]').child(2).click();
+    cy.get('[data-testid=button-card-3]').click();
+    cy.get('[data-testid=input-search]').type('Skol beats').should('have.value', 'Skol beats');
+    cy.get('[data-testid=input-search]').clear('');
+    cy.get('[data-testid=button-card-3]').click();
+    cy.get('[data-testid=decrement-button]').first().click();
+    cy.get('[data-testid=button-card-1]').click();
+    cy.contains('Não foram encontrados produtos disponíveis');
+    cy.get('[data-testid=button-card-0]').click();
   });
 });
