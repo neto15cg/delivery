40c269de3d4e08e7eb6b6d3275c7615c826f5f1c
Eliezer Marques <neto15.cg@gmail.com>
2021-03-08 11:11:42 -0300

    Remove imports nao utilizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Routing from '@routes/Routing';
-import { ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'https://api.code-challenge.ze.delivery/public/graphql',
